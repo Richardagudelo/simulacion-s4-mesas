51d8a67c4bacb3765c1178ec1e2a1ad70db08965
Oscar Rojas <rojascruzoscar@gmail.com>
2021-05-03 09:49:34 -0500

    listo para ci

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const port = process.env.port || 3000;
+app.use(express.json());
+app.use(express.urlencoded());
 
 const logger = require('./logging/logger');
 
