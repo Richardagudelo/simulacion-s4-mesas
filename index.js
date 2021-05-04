73d671e0a568dd3ec2e6055fc3e95728fbc87a44
Richardagudelo <richard.agudelo@uptc.edu.co>
2021-05-03 20:12:38 -0500

    Se arreglo bodyParser

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = process.env.port || 3000;
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({extended: true}));
 
 const logger = require('./logging/logger');
 
